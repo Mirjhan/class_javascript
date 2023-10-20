322998da2d2d8b2378fd59b3877b8e72d252e90d
mirjhan moreyra <mirjhanmoreyra@gmail.com>
2023-10-20 00:25:05 -0500

    Break & continue - Return

---
+++ js_04/return.js
@@ -0,0 +1,19 @@
+function saludar(){
+    return "Hola";
+    console.log("Hola!")
+
+}
+
+console.log(saludar());
+
+function realizarOperacion(op, a, b){
+    switch (op){
+        case "sumar":
+            return parseInt(a) + parseInt (b);
+        case "multiplicar":
+            return a * b;
+    }
+
+}
+console.log(realizarOperacion("sumar", 4, 5));
+console.log(realizarOperacion("multiplicar", 4, 5));
